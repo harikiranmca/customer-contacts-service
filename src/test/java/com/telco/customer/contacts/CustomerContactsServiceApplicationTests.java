f2366add1e342cd9cb13376c387ececdea5043d2
HC <harikiranmca@gmail.com>
2022-07-03 18:03:33 +1000

    Added coverage

--- src/test/java/com/telco/customer/contacts/CustomerContactsServiceApplicationTests.java
+++ src/test/java/com/telco/customer/contacts/CustomerContactsServiceApplicationTests.java
@@ -1,13 +1,14 @@
 package com.telco.customer.contacts;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CustomerContactsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    void contextLoads() {
+    }
 
 }

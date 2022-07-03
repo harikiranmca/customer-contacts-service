6fa393e3dbffd54e1f368c8e018e06d2e4b9c6b0
HC <harikiranmca@gmail.com>
2022-07-03 15:41:16 +1000

    Added gradle and spring boot setup.

---
+++ src/main/java/com/telco/customer/contacts/CustomerContactsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.telco.customer.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerContactsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerContactsServiceApplication.class, args);
+    }
+
+}

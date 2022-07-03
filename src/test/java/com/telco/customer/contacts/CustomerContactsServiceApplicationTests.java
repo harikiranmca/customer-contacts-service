1a7def786ddd26944caeda14f6dc171caada2a40
HC <harikiranmca@gmail.com>
2022-07-03 19:01:04 +1000

    Added rest assured base setup

--- src/test/java/com/telco/customer/contacts/CustomerContactsServiceApplicationTests.java
+++ src/test/java/com/telco/customer/contacts/CustomerContactsServiceApplicationTests.java
@@ -1,14 +1,34 @@
 package com.telco.customer.contacts;
 
-import org.junit.jupiter.api.BeforeAll;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+import static com.jayway.restassured.RestAssured.given;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerContactsServiceApplicationTests {
 
 
-    @Test
-    void contextLoads() {
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach()
+    public void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
     }
 
+    @Test
+    public void makeSureThatGoogleIsUp() {
+
+
+        given().contentType(ContentType.JSON).when().get("/api/v1/phones").then().statusCode(200);
+
+    }
 }
